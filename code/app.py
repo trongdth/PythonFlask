0d2e73e2063ac1e048c607662e17be2831571b09
Trong Dinh <trongdth@gmail.com>
2017-03-18 18:20:16 +0700

    Test #1

--- code/app.py
+++ code/app.py
@@ -14,11 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'trongdth'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-    from db import db
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
