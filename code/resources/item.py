333d5b1e92b292e8cda83e631a4f5e25794a1ee9
Trong Dinh <trongdth@gmail.com>
2017-03-18 18:34:47 +0700

    Test #3

--- code/resources/item.py
+++ code/resources/item.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
-from models.item import ItemModel
+from code.models.item import ItemModel
 
 class Item(Resource):
     parser = reqparse.RequestParser()
--- code/resources/store.py
+++ code/resources/store.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from models.store import StoreModel
+from code.models.store import StoreModel
 
 class Store(Resource):
 
