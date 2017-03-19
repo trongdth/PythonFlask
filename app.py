9aef754f0e34f4d847fb93d662bac549cc5c2e32
Trong Dinh <trongdth@gmail.com>
2017-03-20 01:08:58 +0700

    Test #11

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, request, render_template
+import os
+from flask import Flask, jsonify, request
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
@@ -10,7 +11,7 @@ from resources.store import Store, StoreList
 app = Flask(__name__, template_folder='../templates')
 app.config['DEBUG'] = False
 app.config['JWT_AUTH_HEADER_PREFIX'] = 'Bearer'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'trongdth'
 api = Api(app)
